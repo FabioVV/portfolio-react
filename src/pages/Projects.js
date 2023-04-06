351c5d2f35c1146ac467520ec2d491b96ba3686b
Fábio Varela <fabio-varela@protonmail.com>
2023-04-05 21:50:30 -0300

    Project page start

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import '../styles/projects.css'
+import ProjectItem from '../components/ProjectItem';
+import vcadernoflask from '../assets/vcadernoflask.png'
 
 export default function Project(){
     return (
         <div className='projects'>
             <h1>Some of my personal <span className='title'>Projects</span></h1>
-            <div className='projectList'></div>
+            <div className='projectList'>
+                <ProjectItem name="Flask Note Creator" image={vcadernoflask}/>
+                <ProjectItem name="Placeholder name" image={vcadernoflask}/>
+                <ProjectItem name="Placeholder name" image={vcadernoflask}/>
+            </div>
         </div>
     );
 }
